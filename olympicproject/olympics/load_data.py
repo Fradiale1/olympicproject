586f1aeba5020b7865c49aadd61b86cc85a43e21
Fradiale1 <francesco.adinolfi02@gmail.com>
2024-06-21 12:23:03 +0200

    Update load_data.py

--- olympicproject/olympics/load_data.py
+++ olympicproject/olympics/load_data.py
@@ -5,10 +5,10 @@ from olympicproject.olympics.models import Airline, Airplane, fifa
 
 
 def load_data():
-    olympic_athletes_df = pd.read_csv('olympic_athletes.csv')
-    olympic_hosts_df = pd.read_csv('olympic_hosts.csv')
-    olympic_medals_df = pd.read_csv('olympic_medals.csv')
-    olympic_results_df = pd.read_csv('olympic_results.csv')
+    athletes_df = pd.read_csv('olympic_athletes.csv')
+    hosts_df = pd.read_csv('olympic_hosts.csv')
+    medals_df = pd.read_csv('olympic_medals.csv')
+    results_df = pd.read_csv('olympic_results.csv')
 
     #for _, row in olympic_athletes_df.iterrows():
     #    Airline.objects.create(id=row['id'], name=row['name'], country=row['country'])
