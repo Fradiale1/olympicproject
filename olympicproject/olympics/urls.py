5a8e9c92b3ca7c18fff3bf270236e2e0bd380445
Fradiale1 <francesco.adinolfi02@gmail.com>
2024-06-24 12:08:31 +0200

    Update view

--- olympicproject/olympics/urls.py
+++ olympicproject/olympics/urls.py
@@ -2,10 +2,10 @@
 
 from django.urls import path, include
 from . import views
-from .views.AthleteView import AthleteView
-from .views.HostView import HostView
-from .views.MedalView import MedalView
-from .views.ResultView import ResultView
+from olympics.Views.AthleteView import AthleteView
+from olympics.Views.HostView import HostView
+from olympics.Views.MedalView import MedalView
+from olympics.Views.ResultView import ResultView
 from django_request_mapping import UrlPattern
 
 urlpattern = UrlPattern()
@@ -17,7 +17,9 @@ urlpattern.register(ResultView)
 
 urlpatterns = [
     #path('', views.olympics),
-    
+    path('', views.index, name='index'),
 
+    #Path per la registrazione di tutte le View del Backend funzionante con request mapping. Si crea una classe view, si definiscono le request mapping in questa classe creata
+    #e si registra la view creata in urlpattern
     path('', include(urlpattern)) 
 ]
